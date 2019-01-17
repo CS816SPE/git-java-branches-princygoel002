64b4f1a75b6284992885b7158743b941d4dd14d2
YS3995 <yaminisharma.3995.ys@gmail.com>
2019-01-17 20:25:44 +0530

    addition done

---
+++ calc.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class calc {
+        public static void main(String ar[]) {
+        float a, b, res;
+        char choice, ch;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+                System.out.print("1. ADD TWO NUMBERS\n");
+		System.out.print("2. EXIT\n\n");
+                System.out.print("Enter your choice : ");
+                choice = sc.next().charAt(0);
+                switch(choice) {
+                        case '1' : System.out.print("Enter two numbers : ");
+                        a = sc.nextFloat();
+                        b = sc.nextFloat();
+                        res = a + b;
+                        System.out.print("Result = " + res);
+                        break;
+
+                        case '2' : System.exit(0);
+                                break;
+
+                        default : System.out.print("INVALID CHOIVCE !!!");
+				  break;
+                        }
+                        System.out.print("\n------------------------\n");
+                }while(choice != '2');
+        }
+}
+
