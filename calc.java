e06377e627ddee5ef0bb94464262cddcdcba8473
YS3995 <yaminisharma.3995.ys@gmail.com>
2019-01-17 20:44:23 +0530

    Subtraction done

--- calc.java
+++ calc.java
@@ -8,6 +8,7 @@ public class calc {
         do {
                 System.out.print("1. ADD TWO NUMBERS\n");
 		System.out.print("3. MULTIPLY TWO NUMBERS\n");
+		System.out.print("4. SUBTRACT TWO NUMBERS\n");
 		System.out.print("2. EXIT\n\n");
                 System.out.print("Enter your choice : ");
                 choice = sc.next().charAt(0);
@@ -25,6 +26,14 @@ public class calc {
 				   res = a * b;
 				   System.out.print("Result = " + res);
 				   break;
+
+
+			case '4' : System.out.print("Enter two numbers : ");
+                                   a = sc.nextFloat();
+                                   b = sc.nextFloat();
+                                   res = a - b;
+                                   System.out.print("Result = " + res);
+                                   break;
 
                         case '2' : System.exit(0);
                                 break;
